833518fc9a009ebf2953228677fa0d455e1ce503
sssubham90 <yoyosssubham@gmail.com>
2019-10-05 16:35:37 +0530

    Getting Dummy Data 2

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ let db = require('./bin/dbConnection.js');
 
 let serverConfig = require('./server/config/dbConfig');
 
+let dummyData = require('./dummyData');
+
 db(serverConfig.mongodb.url).then(resolve => {
     console.log(`*********DB is connected successfully*********`);
+    // dummyData.create();
     www();
+
 }).catch(err => {
     console.log(`*********err*********\n${err}`);
 });
