e23725bb2364834a13493798e122d91359d108c9
sssubham90 <yoyosssubham@gmail.com>
2019-12-09 22:53:58 +0530

    Changed the msg91 Key

--- server/services/OTPService.js
+++ server/services/OTPService.js
@@ -1,4 +1,4 @@
 const SendOtp = require('sendotp');
-const sendOtp = new SendOtp('299694AvNkWpxlEzrS5daa170b', 'Please use this OTP: {{otp}} AksDai');
+const sendOtp = new SendOtp('274915AKKzGiEvpfL05ccb145a', 'Please use this OTP: {{otp}} AksDai'); //299694AvNkWpxlEzrS5daa170b
 
 module.exports = sendOtp;
